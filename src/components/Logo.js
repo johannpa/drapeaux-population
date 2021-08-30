84453e02b6fc3bb294ad52326e7f55d0a1e5d88d
johannpa <johann_paque@hotmail.fr>
2021-08-30 14:50:54 +0200

    Add files via upload

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const logo = () => {
+    return ( 
+        <div>
+            <div className="logo">
+               <img src="./img/logo192.png" alt="logo" /> 
+               <h3>React World</h3>
+            </div>
+        </div>
+    );
+};
+
+export default logo;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return ( 
+        <div>
+            <Navigation />
+            <Logo/>
+            <h1>À propos</h1>
+            <br/>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolore reiciendis rem! Magnam eveniet, tenetur eum quis perspiciatis, iure doloribus tempora aut quos doloremque id qui numquam? Voluptas quos consequuntur praesentium molestias sint debitis eius saepe distinctio commodi. Minus necessitatibus autem dolorum. Unde laborum repellat et rem, excepturi optio quaerat rerum odio? Porro dolore exercitationem cum sapiente! Rerum, officia adipisci.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus earum illum est. Beatae minima architecto laboriosam minus alias sed facere, inventore tenetur repellendus? Fugit in sequi pariatur cupiditate ex repellendus soluta provident, quaerat accusamus, quidem ipsa voluptas autem reiciendis deserunt blanditiis accusantium ullam debitis commodi non. At commodi cumque repudiandae nihil possimus iste, libero enim officiis id beatae dolorum sit, soluta in! Explicabo nobis impedit similique? Praesentium, recusandae porro natus quas debitis perspiciatis laudantium iure aspernatur aliquam voluptate dolores provident id, soluta fugiat quidem dicta nulla nisi iusto sapiente dolore, voluptatum consequuntur exercitationem vel. Quas earum blanditiis soluta accusantium iure debitis, mollitia numquam nisi voluptate harum autem provident fugiat, doloremque dolore natus laborum ut quos excepturi quam at? Eos, reiciendis iure quibusdam qui, nemo repellendus eius alias voluptatem ad cum, necessitatibus illum esse? Consectetur repellendus fuga itaque sit cumque blanditiis?</p>
+        </div>
+    );
+};
+
+export default About;
